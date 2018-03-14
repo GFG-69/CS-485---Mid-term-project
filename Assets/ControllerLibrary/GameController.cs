398b21507b60d58cfe3acdf3d0e5f9033725d793
root <root@DESKTOP-KUCD4AP.localdomain>
2018-03-14 04:05:55 +0100

    push

--- Assets/ControllerLibrary/GameController.cs
+++ Assets/ControllerLibrary/GameController.cs
@@ -7,7 +7,9 @@ public class GameController : MonoBehaviour {
 	private float TimeOfSlowing;
 
 	public float Intensity;
-	public float Duration;
+	public float DurationOfRising;
+	public float DurationOfPlateau;
+	public float DurationOfDecreasing;
 
 	// Use this for initialization
 	void Start () {
@@ -16,14 +18,19 @@ public class GameController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (TimeOfSlowing - Time.time > 0)
-			Time.timeScale = 1.0f - ((TimeOfSlowing - Time.time) / Duration * Intensity);
+		if (TimeOfSlowing - Time.time > DurationOfRising + DurationOfPlateau)
+			Time.timeScale = 1.0f - (DurationOfRising / (DurationOfRising - TimeOfSlowing - DurationOfDecreasing - DurationOfPlateau + Time.time) * Intensity);
+		else if (TimeOfSlowing - Time.time > DurationOfPlateau)
+			Time.timeScale = 1.0f - Intensity;
+		else if (TimeOfSlowing - Time.time > 0)
+			Time.timeScale = 1.0f - ((TimeOfSlowing - Time.time) / DurationOfDecreasing * Intensity);
 		else
 			Time.timeScale = 1.0f;
+		Debug.Log (Time.time);
 	}
 
 	public void SlowTheTime() {
-		TimeOfSlowing = Time.time + Duration;
+		TimeOfSlowing = Time.time + DurationOfRising + DurationOfPlateau + DurationOfDecreasing;
 	}
 
 }
